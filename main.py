0c5794564388d1eebe4125d1dba02b9dd67b6b70
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2024-11-01 16:21:02 +0100

    feat add: use Matplotlib for plotting monthly expense data

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import csv
 from datetime import datetime 
 from data_entry import get_date, get_amount, get_category, get_description
+import matplotlib.pyplot as plt
 
 class CSV:
     CSV_FILE = 'finance_data.csv'
@@ -64,8 +65,26 @@ def add():
 
     CSV.add_entry(date, amount, category, description)
 
+def plot_transaction(df):
+    df.set_index('date', inplace=True)
+
+    income_df = df[df['category'] == 'Income'].resample('D').sum().reindex(df.index, fill_value=0)
+    expense_df = df[df['category'] == 'Expense'].resample('D').sum().reindex(df.index, fill_value=0)
+
+    plt.figure(figsize=(10, 5))
+    plt.title('Income and Expense Over Time')
+    plt.plot(income_df.index, income_df['amount'], label='Income', color='g')
+    plt.plot(expense_df.index, expense_df['amount'], label='Expense', color='r')
+    plt.xlabel('Date')
+    plt.ylabel('Amount')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
 def main():
     while True:
+        print('\n')
+        print('Choose from below:')
         print('1. Add transaction')
         print('2. View transaction summary within a date range')
         print('3. Exit')
@@ -77,7 +96,10 @@ def main():
         elif choice == '2':
             start_date = get_date('Enter the start date (dd-mm-yyyy): ')
             end_date = get_date('Enter the end date (dd-mm-yyyy): ')
-            CSV.get_transaction(start_date, end_date)
+            df = CSV.get_transaction(start_date, end_date)
+            if input('Do you want to see a plot? (y/n): ').lower() == 'y':
+                plot_transaction(df)
+
         elif choice == '3':
             print('Exiting!')
             break
